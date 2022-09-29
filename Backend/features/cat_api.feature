72473a722a4a54fb9ed7f027793ba1b8a918b7a8
Igor Santos <ins@cesar.org.br>
2022-09-29 15:00:57 -0300

    Parte final Cats

--- Backend/features/cat_api.feature
+++ Backend/features/cat_api.feature
@@ -2,13 +2,13 @@ Feature: Cat Fact API
     An API for facts about cats
 
     Scenario: Get random fact
-        When I send a GET request to fact
-        Then the API returns response 200
+        Given I send a GET request to fact
+        Then The API returns response 200
 
     Scenario: Get random facts
-        When I send a GET request to facts
-        Then the API returns response 200
+        Given I send a GET request to facts
+        Then The API returns response 200
 
     Scenario: Get list of breeds
-        When I send a GET request to breeds
-        Then the API returns response 200
+        Given I send a GET request to breeds
+        Then The API returns response 200
--- Backend/steps/cat_fact_steps.py
+++ Backend/steps/cat_fact_steps.py
@@ -1,15 +1,23 @@
 from behave import step
-from requests import request
+from nose.tools import assert_equal
+import requests
+
 
 @step('I send a GET request to fact')
 def send_get_fact(context):
-    requests.get ()
+    context.response = requests.get('https://catfact.ninja/fact')
+
 
 @step('I send a GET request to facts')
 def send_get_fact(context):
+    context.response = requests.get('https://catfact.ninja/facts')
+
 
 @step('I send a GET request to breeds')
 def send_get_fact(context):
+    context.response = requests.get('https://catfact.ninja/breeds')
 
-@step('the API returns response 200')
-def send_get_fact(context):
+
+@step('the API returns response {status_code}')
+def send_get_fact(context, status_code):
+    assert_equal(context.response.status_code, int(status_code))
