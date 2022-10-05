d2f81adcd7e00769b578c963029145470f91a7be
brunorafaelpb <brunorafael1992@gmail.com>
2022-10-05 16:03:43 -0300

    parte 4

--- Backend/features/coffee.feature
+++ Backend/features/coffee.feature
@@ -1,4 +1,11 @@
 Feature: Get Coffee
-
+    @esse
     Scenario: Get Coffee Image
-        Given I send a GET request to coffee
+        Given I send a GET request to Coffee image
+        Then The API returns response 200
+        And The returned image is correct
+
+    Scenario: Get Coffee Image JSON
+        Given I send a GET request to Coffee JSON
+        Then The API returns response 200
+        And The returned JSON is correct
---
+++ Backend/steps/coffee_steps.py
@@ -0,0 +1,15 @@
+from behave import step
+from nose.tools import assert_equal, assert_is_not_none
+import requests
+
+@step('I send a GET request to Coffee image')
+def send_get_fact(context):
+    context.response = requests.get('https://coffee.alexflipnote.dev/random')
+
+@step('I send a GET request to Coffee JSON')
+def send_get_fact(context):
+    context.response = requests.get('https://coffee.alexflipnote.dev/random.json')
+
+@step('The returned image is correct')
+def step_impl(context):
+    assert_is_not_none(context.response.content)
